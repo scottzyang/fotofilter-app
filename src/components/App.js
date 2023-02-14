845c8f624479d3b5f4431e32aa28c8c26691e083
scottzyang <scottzyang@gmail.com>
2023-02-14 15:24:06 -0600

    Modify App to outlet

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ImageFilter from 'react-image-filter';
+// import ImageFilter from 'react-image-filter';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-        <ImageFilter
-        image='https://source.unsplash.com/random/1200x800'
-        filter={ 'duotone' }
-        colorOne={ [40, 250, 250] }
-        colorTwo={ [250, 150, 0] }
-      />
-
+      <Outlet />
     </div>
   );
 }
 
 export default App;
+      /* <ImageFilter
+        image='https://source.unsplash.com/random/1200x800'
+        filter={ 'duotone' }
+        colorOne={ [40, 250, 250] }
+        colorTwo={ [250, 150, 0] }
+      /> */

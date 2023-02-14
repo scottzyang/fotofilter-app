97d46e5657b00f9c82350ab817f586c8b9f9b99b
scottzyang <scottzyang@gmail.com>
2023-02-14 15:22:13 -0600

    Add home page component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h1>homepage</h1>
+      <Link to={`/select-image`}>Use Provided Images</Link>
+      <Link to={`/select-image`}>Upload Local Image</Link>
+    </div>
+  )
+}
+
+export default Home

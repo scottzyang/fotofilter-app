a530bea3266d8691ea4542f8717c9feba2c452a8
scottzyang <scottzyang@gmail.com>
2023-02-20 15:49:26 -0600

    Modify favicon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import Home from './components/Home/Home';
 import ImageTilesDisplay from './components/ImageTilesDisplay/ImageTilesDisplay';
 import Upload from './components/Upload/Upload';
+import Editor from './components/Editor/Editor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -17,6 +18,7 @@ root.render(
           <Route path='/' element={<Home />}/>
           <Route path='/select-image' element={<ImageTilesDisplay />}/>
           <Route path='/upload-image' element={<Upload />}/>
+          <Route path='/editor' element={<Editor />}/>
         </Route>
       </Routes>
     </Router>

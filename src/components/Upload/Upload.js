870f3fe87a980f11514819213bf21f0e61985dff
scottzyang <scottzyang@gmail.com>
2023-02-20 21:29:43 -0600

    Add logic to upload file from local

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,14 +1,16 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import './Upload.css'
-import { useState } from 'react';
+import { useOutletContext, Link } from 'react-router-dom';
 
 function Upload() {
-  const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedImage, setSelectedImage] = useOutletContext()
+
   return(
     <div>
       <h1>Upload Your Own Image</h1>
       {selectedImage && (
         <div>
-        <img alt="Uploaded file not found" width={"350px"} src={URL.createObjectURL(selectedImage)} />
+        <img alt="Uploaded file not found" width={"350px"} src={selectedImage} />
         <br />
         <button onClick={()=>setSelectedImage(null)}>Remove</button>
         </div>
@@ -18,12 +20,17 @@ function Upload() {
       <br /> 
       <input
         type="file"
+        accept="image/*"
+        capture='user'
         name="myImage"
         onChange={(event) => {
           console.log(event.target.files[0]);
-          setSelectedImage(event.target.files[0]);
+          setSelectedImage(URL.createObjectURL(event.target.files[0]));
         }}
       />
+      <br />
+      <br />
+      {selectedImage && <button><Link to={`/editor`}>Start Editing!</Link></button>}
     </div>
   )
 }

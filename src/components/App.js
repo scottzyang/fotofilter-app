5e20fcafbd28505025ccee8a74ca2609099d82ec
scottzyang <scottzyang@gmail.com>
2023-02-21 14:37:28 -0600

    "Add footer component to app"

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Outlet, } from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
 import './App.css';
+import Footer from './Footer/Footer'
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Outlet context={[selectedImage, setSelectedImage]}/>
       {console.log(selectedImage)}
+      <Footer />
     </div>
   );
 }

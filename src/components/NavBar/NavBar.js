35233a3faa2ba0453c5d2bc475f2ad606393c700
scottzyang <scottzyang@gmail.com>
2023-02-21 14:36:34 -0600

    "Add styling via tailwindcss"

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,14 +4,13 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return(
     <div className='NavBar'>
-      <header>
-        <h1>Foto Filter</h1>
+      <header className="bg-indigo-300 h-40 flex flex-col justify-center">
+        <h1 className="text-5xl font-mono m-2">Foto Filter</h1>
         <p className="NavBar-Subtitle">Foto Filtering Fun</p>
+        <nav>
+          <NavLink className={({ isActive }) => isActive? "nav-link-active" : 'nav-link'} to='/'>HOME</NavLink>
+        </nav>
       </header>
-      
-      <nav>
-        <NavLink className={({ isActive }) => isActive? "nav-link-active" : 'nav-link'} to='/'>HOME</NavLink>
-      </nav>
     </div>
   )
 }

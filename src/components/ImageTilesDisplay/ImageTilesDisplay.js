654ffbe811afbacead98f4337a7769f9788f7ab1
scottzyang <scottzyang@gmail.com>
2023-02-20 21:32:12 -0600

    Set up state outlet context and pass set function into image tile

--- src/components/ImageTilesDisplay/ImageTilesDisplay.js
+++ src/components/ImageTilesDisplay/ImageTilesDisplay.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './ImageTilesDisplay.css'
 import ImageTile from "../ImageTile/ImageTile";
+import { useOutletContext } from "react-router-dom";
 import data from '../../data.json'
 
 function ImageTilesDisplay() {
+  // eslint-disable-next-line no-unused-vars
+  const [selectedImage, setSelectedImage] = useOutletContext()
   const imageGrid = data.map(({ title, imageUrl }) => {
-    return (<ImageTile imageUrl={imageUrl} title={title}/>)
+    return (<ImageTile key={imageUrl} imageUrl={imageUrl} title={title} setSelectedImage={setSelectedImage}/>)
   })
   return (
     <div className="ImageTilesDisplay">

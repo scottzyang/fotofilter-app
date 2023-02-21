755163ac8420317bc8d2c1356127e36de950e27b
scottzyang <scottzyang@gmail.com>
2023-02-21 14:36:14 -0600

    "Add styling via tailwind and placeholder image"

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -6,31 +6,39 @@ function Upload() {
   const [selectedImage, setSelectedImage] = useOutletContext()
 
   return(
-    <div>
+    <div className="flex flex-col justify-center items-center">
       <h1>Upload Your Own Image</h1>
+      {!selectedImage && 
+        <div className="bg-gray-300 border-black border-4 w-500 h-500 rounded-2xl">
+          <img src={`${process.env.PUBLIC_URL}/images/no-pictures.png`} alt="selected" width="400"/>
+        </div>
+      }
       {selectedImage && (
-        <div>
-        <img alt="Uploaded file not found" width={"350px"} src={selectedImage} />
-        <br />
-        <button onClick={()=>setSelectedImage(null)}>Remove</button>
+        <div className="flex flex-col justify-center items-center">
+          <img alt="Uploaded file not found" width={"400px"} src={selectedImage} />
+          <button className="border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5" onClick={()=>setSelectedImage(null)}>Remove</button>
         </div>
       )}
-      <br />
-     
-      <br /> 
-      <input
-        type="file"
-        accept="image/*"
-        capture='user'
-        name="myImage"
-        onChange={(event) => {
-          console.log(event.target.files[0]);
-          setSelectedImage(URL.createObjectURL(event.target.files[0]));
-        }}
-      />
-      <br />
-      <br />
-      {selectedImage && <button><Link to={`/editor`}>Start Editing!</Link></button>}
+      <div className='m-4'>
+        <input
+          className="block w-full text-sm text-slate-500
+          file:mr-4 file:py-2 file:px-4
+          file:rounded-full file:border-0
+          file:text-sm file:font-semibold
+          file:bg-violet-50 file:text-violet-700
+          hover:file:bg-violet-100"
+          type="file"
+          accept="image/*"
+          capture='user'
+          name="myImage"
+          onChange={(event) => {
+            console.log(event.target.files[0]);
+            setSelectedImage(URL.createObjectURL(event.target.files[0]));
+          }}
+        />
+      </div>
+
+      {selectedImage && <button className="border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5"><Link to={`/editor`}>Start Editing!</Link></button>}
     </div>
   )
 }

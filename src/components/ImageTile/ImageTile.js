8f3c182eb1f7aa097f32e8f1bef70f83db886d75
scottzyang <scottzyang@gmail.com>
2023-02-20 21:32:57 -0600

    Pass use state set function and set image to user selected

--- src/components/ImageTile/ImageTile.js
+++ src/components/ImageTile/ImageTile.js
@@ -2,10 +2,10 @@ import './ImageTile.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function ImageTile({ imageUrl, title }) {
+function ImageTile({ imageUrl, title, setSelectedImage }) {
   return(
     <div className='ImageTile'>
-      <Link>
+      <Link onClick={() => setSelectedImage(`images/${imageUrl}`)}>
         <img src={`${process.env.PUBLIC_URL}/images/${imageUrl}`} alt={title}/>
       </Link>
     </div>

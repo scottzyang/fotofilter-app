a23d73c02fb612399a84ad113385bcbf6adaaeeb
scottzyang <scottzyang@gmail.com>
2023-02-21 14:35:02 -0600

    "Add tailwind styling and placeholder image"

--- src/components/ImageTilesDisplay/ImageTilesDisplay.js
+++ src/components/ImageTilesDisplay/ImageTilesDisplay.js
@@ -11,17 +11,28 @@ function ImageTilesDisplay() {
     return (<ImageTile key={imageUrl} imageUrl={imageUrl} title={title} setSelectedImage={setSelectedImage}/>)
   })
   return (
-    <div className="ImageTilesDisplay">
+    <div className="flex flex-col justify-center items-center">
       
+      {!selectedImage && 
+        <>
+          <h3>Selected Photo:</h3>
+          <div className="bg-gray-300 border-black border-4 w-500 h-500 rounded-2xl">
+            <img src={`${process.env.PUBLIC_URL}/images/no-pictures.png`} alt="selected" width="400"/>
+          </div>
+          <button className="border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5"><Link to={`/editor`}>Start Editing!</Link></button>
+        </>
+      }
+    
       {selectedImage && 
         <>
           <h3>Selected Photo:</h3>
-          <img src={selectedImage} alt="selected" width="500"/>
-          <br/>
-          <button><Link to={`/editor`}>Start Editing!</Link></button>
+          <div className="border-black border-4 w-500 rounded-2xl">
+            <img className="rounded-xl" src={selectedImage} alt="selected" width="400"/>
+          </div>
+          <button className="border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5"><Link to={`/editor`}>Start Editing!</Link></button>
         </>
       }
-      <div className="image-grid">
+      <div className="grid grid-cols-4 gap-6">
         { imageGrid }
       </div>
     </div>

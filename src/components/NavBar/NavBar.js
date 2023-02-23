1ca4ea26ba4c65ac0cc7d5a7b9a56db165da3428
scottzyang <scottzyang@gmail.com>
2023-02-23 15:24:01 -0600

    Change title of app

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return(
     <div className='NavBar'>
       <header className="bg-indigo-300 h-40 flex flex-col justify-center">
-        <h1 className="text-5xl font-mono m-2 font-bold">Foto Filter</h1>
+        <h1 className="text-5xl font-mono m-2 font-bold">FotoFilter</h1>
         <p className="NavBar-Subtitle">Foto Filtering Fun</p>
         <nav className="m-4">
           <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/'>Home</NavLink>

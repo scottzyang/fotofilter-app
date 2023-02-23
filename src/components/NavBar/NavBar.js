c3eda0118bcb29384b27c5ead39cc9878703b841
scottzyang <scottzyang@gmail.com>
2023-02-23 14:25:12 -0600

    Style and reorganize navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,10 +5,12 @@ function NavBar() {
   return(
     <div className='NavBar'>
       <header className="bg-indigo-300 h-40 flex flex-col justify-center">
-        <h1 className="text-5xl font-mono m-2">Foto Filter</h1>
+        <h1 className="text-5xl font-mono m-2 font-bold">Foto Filter</h1>
         <p className="NavBar-Subtitle">Foto Filtering Fun</p>
-        <nav>
-          <NavLink className={({ isActive }) => isActive? "nav-link-active" : 'nav-link'} to='/'>HOME</NavLink>
+        <nav className="m-4">
+          <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/'>Home</NavLink>
+          <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/upload-image'>Upload</NavLink>
+          <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/select-image'>Select</NavLink>
         </nav>
       </header>
     </div>

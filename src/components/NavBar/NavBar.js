14751d29f6055365f8fe75a609cd0fdf34293fbb
scottzyang <scottzyang@gmail.com>
2023-02-23 15:49:43 -0600

    Remove subtitle from header

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,11 +6,10 @@ function NavBar() {
     <div className='NavBar'>
       <header className="bg-indigo-300 h-40 flex flex-col justify-center">
         <h1 className="text-5xl font-mono m-2 font-bold">FotoFilter</h1>
-        <p className="NavBar-Subtitle">Foto Filtering Fun</p>
         <nav className="m-4">
-          <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/'>Home</NavLink>
-          <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/upload-image'>Upload</NavLink>
-          <NavLink className="border-black border-4 rounded-2xl py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/select-image'>Select</NavLink>
+          <NavLink className="border-black border-4 rounded-lg py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/'>Home</NavLink>
+          <NavLink className="border-black border-4 rounded-lg py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/select-image'>Select</NavLink>
+          <NavLink className="border-black border-4 rounded-lg py-1 px-4 bg-indigo-500/75 hover:opacity-75 m-2" to='/upload-image'>Upload</NavLink>
         </nav>
       </header>
     </div>

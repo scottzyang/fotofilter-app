47ad9ed64e3b0390bdeeeee736c9b091d326e38d
scottzyang <scottzyang@gmail.com>
2023-02-23 17:17:15 -0600

    Add responsiveness to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Home.css'
 import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <section className="flex justify-center">
-      <section className="grid gap-14 grid-cols-[300px_300px] m-12">
+      <section className="home-button grid gap-14 grid-cols-[300px_300px] m-12">
         <section className="flex flex-col justify-center">
           <p className="font-sans m-4 text-xl font-semibold">Select an Image</p>
           <section className="bg-yellow-100 border-black border-4 rounded-2xl w-84 hover:opacity-75">

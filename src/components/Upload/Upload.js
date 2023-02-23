88691436989f0c9c93b6020bdea4c6514ee03796
scottzyang <scottzyang@gmail.com>
2023-02-23 14:24:57 -0600

    Refactor upload webpage organization

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,5 +1,4 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import './Upload.css'
 import { useOutletContext, Link } from 'react-router-dom';
 
 function Upload() {
@@ -7,17 +6,19 @@ function Upload() {
 
   return(
     <div className="flex flex-col justify-center items-center">
-      <h1>Upload Your Own Image</h1>
+      <h1 className="text-xl m-2 font-semibold">Upload Your Own Image</h1>
       {!selectedImage && 
         <div className="bg-gray-300 border-black border-4 w-500 h-500 rounded-2xl">
           <img src={`${process.env.PUBLIC_URL}/images/no-pictures.png`} alt="selected" width="400"/>
         </div>
       }
       {selectedImage && (
-        <div className="flex flex-col justify-center items-center">
-          <img alt="Uploaded file not found" width={"400px"} src={selectedImage} />
-          <button className="border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5" onClick={()=>setSelectedImage(null)}>Remove</button>
-        </div>
+        <>
+          <div className="fbg-gray-300 border-black border-4 w-500 h-500 rounded-2xl">
+            <img className="rounded-xl" alt="Uploaded file not found" width={"400px"} src={selectedImage} />
+          </div>
+        </>
+
       )}
       <div className='m-4'>
         <input
@@ -37,8 +38,10 @@ function Upload() {
           }}
         />
       </div>
-
-      {selectedImage && <button className="border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5"><Link to={`/editor`}>Start Editing!</Link></button>}
+      <div className='m-8'>
+        <Link style={!selectedImage ? {pointerEvents: 'none'} : {pointerEvents: ''}} className={!selectedImage ? "border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 opacity-50 m-5" : "border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5"} to={`/editor`}>Start Editing!</Link>
+        <Link style={!selectedImage ? {pointerEvents: 'none'} : {pointerEvents: ''}} className={!selectedImage ? "border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 opacity-50 m-5" : "border-black border-4 rounded-2xl py-2 px-4 bg-indigo-500/75 hover:opacity-75 m-5"} onClick={() => setSelectedImage(null)}>Remove</Link>
+      </div>
     </div>
   )
 }
